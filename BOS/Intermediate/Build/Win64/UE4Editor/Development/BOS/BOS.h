9212cb247a7512a98fce1bfdf2e1875bf331ebce
ankipanki123 <henriknorinder_88@hotmail.com>
2015-11-06 13:32:37 +0100

    Booster

--- BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/BOS.h
+++ BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/BOS.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_BOS_H__
 #define __AUTO_BOS_H__
-//Last Write: 2015-11-04 19:52:46 +00:00
-#include "C:\Users\Stoffe\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
+//Last Write: 2015-10-27 11:44:49 +00:00
+#include "C:\Users\Henrik\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
 #endif//__AUTO_BOS_H__
--- BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/PCH.BOS.h.cpp
+++ BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/PCH.BOS.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\Stoffe\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
+#include "C:\Users\Henrik\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
