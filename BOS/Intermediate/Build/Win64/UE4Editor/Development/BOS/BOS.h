ea3c3b3ec47501d11f85337f3964905d5a68c5b9
Stiffrock <christofer.malmberg@gmail.com>
2015-11-11 12:24:03 +0100

    friction

--- BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/BOS.h
+++ BOS/Intermediate/Build/Win64/UE4Editor/Development/BOS/BOS.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_BOS_H__
 #define __AUTO_BOS_H__
-//Last Write: 2015-11-04 20:48:01 +00:00
-#include "C:\Users\Stoffe\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
+//Last Write: 2015-10-30 11:16:36 +00:00
+#include "C:\Users\Spellabbet\Documents\GitHub\Balls-of-steel\BOS\Source\BOS\BOS.h"
 #endif//__AUTO_BOS_H__
